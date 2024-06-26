d53d3621d4a507d865a78736dc99164a130cdb75
Azhar Abbass <azharabbass786.com@gmail.com>
2024-06-27 02:57:09 +0500

    First 5 lectures completed

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+<h1>About page welcome back.</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+<x-slot:heading>
+        Home Page
+    </x-slot:heading>
+<h1>Home page welcome back.</h1>
+</x-layout>
